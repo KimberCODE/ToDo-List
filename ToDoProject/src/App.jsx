589d5ac6b1e82686ce67ce4723b55d5bd4f392ea
KimberCODE <kmendoza.prsvr@gmail.com>
2024-06-12 10:26:10 -0500

    Brainstorming Framework

--- ToDoProject/src/App.jsx
+++ ToDoProject/src/App.jsx
@@ -1,6 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 
+
+//Objective: We are creating a To Do list Application.
+
+//**Criteria:
+//1- Date and Time for the deadline
+//2- Add/Complete/Delete task (radio button for complete/input for add)
+//3- Save tasks to repopulate when app closes/reopens --- local storage (research task)
+//4- Store completed tasks with undo (toggle)
+//5- Work/Personal separate lists (functional buttons to )
+
+//**Components:
+  //1- TaskList - ul,
+  //2- ListItem
+  //3- ListToggle
+  //4- AddButton
+
+
 function App() {
 
   return (
